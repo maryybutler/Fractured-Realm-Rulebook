a37f8a8aafe4a617d151fbd5b926f3803796499b
maryybutler <maryybutler23@gmail.com>
2025-11-14 14:54:40 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+// Minimal service worker - helps PWA installability and basic offline behavior
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
 
